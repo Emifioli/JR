07f4b982aac1684598c595b365c7f28d681963be
Emifioli <Emifioli@mail.ru>
2015-06-10 20:55:29 +0300

    tsk

--- src/com/javarush/test/level22/lesson09/task03/Solution.java
+++ src/com/javarush/test/level22/lesson09/task03/Solution.java
@@ -1,5 +1,11 @@
 package com.javarush.test.level22.lesson09.task03;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 /* Составить цепочку слов
 В методе main считайте с консоли имя файла, который содержит слова, разделенные пробелом.
 В методе getLine используя StringBuilder расставить все слова в таком порядке,
@@ -8,7 +14,6 @@ package com.javarush.test.level22.lesson09.task03;
 Метод getLine должен возвращать любой вариант.
 Слова разделять пробелом.
 В файле не обязательно будет много слов.
-
 Пример тела входного файла:
 Киев Нью-Йорк Амстердам Вена Мельбурн
 
@@ -16,13 +21,46 @@ package com.javarush.test.level22.lesson09.task03;
 Амстердам Мельбурн Нью-Йорк Киев Вена
 */
 public class Solution {
-    public static void main(String[] args) {
-        //...
-        StringBuilder result = getLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()));
+        String line = "";
+        ArrayList<String>list = new ArrayList<String>();
+        while ((line=fileReader.readLine())!=null){
+            String x[] = line.split(" ");
+            for(String y : x){
+                list.add(y);
+            }
+        }
+        String y [] = new String[list.size()];
+        for(int i=0;i<list.size();i++){
+            y[i]=list.get(i);
+        }
+
+        StringBuilder result = getLine(y);
         System.out.println(result.toString());
+
     }
 
     public static StringBuilder getLine(String... words) {
-          return null;
+          StringBuilder x = new StringBuilder();
+          for(int i=0;i<words.length;i++){
+               for(int k=words.length-1;k>i;k--){
+                   char one = words[i].toLowerCase().charAt(words[i].length()-1);
+                   char two = words[k].toLowerCase().charAt(0);
+                   if(one==two){
+                       String qwe = words[i+1];
+                       words[i+1]=words[k];
+                       words[k]=qwe;
+                   }
+               }
+          }
+           for(int i=0;i<words.length;i++){
+               if(i==words.length-1)
+                   x.append(words[i]);
+               else x.append(words[i]+" ");
+           }
+
+          return x;
     }
 }
