b0bf6ee4331c4fa27fc3976d3a973baec7cb0635
Emifioli <Emifioli@mail.ru>
2015-06-14 19:24:09 +0300

    tsk

--- src/com/javarush/test/level22/lesson18/big01/Field.java
+++ src/com/javarush/test/level22/lesson18/big01/Field.java
@@ -1,4 +1,22 @@
 package com.javarush.test.level22.lesson18.big01;
 
 public class Field {
+    int width;
+    int height;
+    int[][]matrix;
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public int[][] getMatrix()
+    {
+        return matrix;
+    }
 }
