67584ab4fd6a7b0fe1b5ce332ad0f44de05cdbe6
Emifioli <Emifioli@mail.ru>
2015-06-14 19:17:48 +0300

    tsk

--- src/com/javarush/test/level22/lesson18/big01/Tetris.java
+++ src/com/javarush/test/level22/lesson18/big01/Tetris.java
@@ -4,4 +4,17 @@ public class Tetris {
     public static void main(String[] args) {
 
     }
+    Field field;
+    Figure figure;
+
+    public Field getField()
+    {
+        return field;
+    }
+
+    public Figure getFigure()
+    {
+        return figure;
+    }
+
 }
