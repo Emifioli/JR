29e6007945ad5ae85c3b45bbc1e7f87524de11aa
Emifioli <Emifioli@mail.ru>
2015-06-14 19:40:23 +0300

    tsk

--- src/com/javarush/test/level22/lesson18/big01/Figure.java
+++ src/com/javarush/test/level22/lesson18/big01/Figure.java
@@ -1,4 +1,22 @@
 package com.javarush.test.level22.lesson18.big01;
 
 public class Figure {
+    int x;
+    int y;
+    int[][]matrix;
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public int[][] getMatrix()
+    {
+        return matrix;
+    }
 }
