d97889efcb568817d4a01ec5a5c58cecf955fdc8
Emifioli <Emifioli@mail.ru>
2015-06-14 11:04:03 +0300

    tsk

--- src/com/javarush/test/level23/lesson06/task02/Solution.java
+++ src/com/javarush/test/level23/lesson06/task02/Solution.java
@@ -8,44 +8,47 @@ public class Solution {
 
     public class ServerNotAccessibleException extends Exception {
         public ServerNotAccessibleException() {
-            super("Server is not accessible for now.");
+            super(Constants.Server_is_not_accessible_for_now);
         }
 
         public ServerNotAccessibleException(Throwable cause) {
-            super("Server is not accessible for now.", cause);
+            super(Constants.Server_is_not_accessible_for_now, cause);
         }
     }
 
     public class UnauthorizedUserException extends Exception {
         public UnauthorizedUserException() {
-            super("User is not authorized.");
+            super(Constants.User_is_not_authorized);
         }
 
         public UnauthorizedUserException(Throwable cause) {
-            super("User is not authorized.", cause);
+            super(Constants.User_is_not_authorized, cause);
         }
     }
 
     public class BannedUserException extends Exception {
         public BannedUserException() {
-            super("User is banned.");
+            super(Constants.User_is_banned);
         }
 
         public BannedUserException(Throwable cause) {
-            super("User is banned.", cause);
+            super(Constants.User_is_banned, cause);
         }
     }
 
     public class RestrictionException extends Exception {
         public RestrictionException() {
-            super("Access is denied.");
+            super(Constants.Access_is_denied);
         }
 
         public RestrictionException(Throwable cause) {
-            super("Access is denied.", cause);
+            super(Constants.Access_is_denied, cause);
         }
     }
-    public final class Constants{
-
+    public static final class Constants{
+        public static final String Server_is_not_accessible_for_now = "Server is not accessible for now.";
+        public static final String User_is_not_authorized = "User is not authorized.";
+        public static final String User_is_banned = "User is banned.";
+        public static final String Access_is_denied = "Access is denied.";
     }
 }
