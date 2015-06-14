b19a9b62730c725f79b5c3e700e75b24149f6951
Emifioli <Emifioli@mail.ru>
2015-06-14 19:22:04 +0300

    tsk

--- src/com/javarush/test/level22/lesson18/big01/Tetris.java
+++ src/com/javarush/test/level22/lesson18/big01/Tetris.java
@@ -2,10 +2,13 @@ package com.javarush.test.level22.lesson18.big01;
 
 public class Tetris {
     public static void main(String[] args) {
-
+        game = new Tetris();
+        game.run();
     }
+
     Field field;
     Figure figure;
+    public static Tetris game;
 
     public Field getField()
     {
